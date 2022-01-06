9413ad8f362c6fc9a5e37f271f0fe52666d4dcd6
Daniel Phan <danielphan.2003@gmail.com>
2022-01-06 16:27:57 +0700

    papermc: adoptopenjdk-jre-openj9-bin-16 for 1.18

--- src/pkgs/games/papermc/utils.nix
+++ src/pkgs/games/papermc/utils.nix
@@ -5,6 +5,7 @@
 
 , jre8
 , jre_headless
+, adoptopenjdk-jre-openj9-bin-16
 , javaPackages
 }: pkgSetUtils // rec {
   # based on https://www.creeperhost.net/wiki/books/minecraft-java-edition/page/changing-java-versions
@@ -12,7 +13,7 @@
     isMcVer = mcVer': builtins.compareVersions mcVer mcVer' == -1;
   in
     if isMcVer "1.17" then jre8
-    else if isMcVer "1.18" then javaPackages.compiler.openjdk16.headless
+    else if isMcVer "1.18" then adoptopenjdk-jre-openj9-bin-16
     else if isMcVer "1.19" then javaPackages.compiler.openjdk17.headless
     else jre_headless;
 
